c44878d83bd03e665e61f0129b8c6e33f36b5384
Nathanael Vieira <59123681+NathanaelV@users.noreply.github.com>
2022-08-23 16:03:32 -0300

    lib/rotakawasaki

--- lib/rotakawasaki.rb
+++ lib/rotakawasaki.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
-require_relative "rotakawasaki/version"
+require_relative 'rotakawasaki/version'
+require 'f1sales_custom/parser'
+require 'f1sales_custom/source'
+require 'f1sales_custom/hooks'
 
 module Rotakawasaki
   class Error < StandardError; end
