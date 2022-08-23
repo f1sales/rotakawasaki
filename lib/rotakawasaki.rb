7ad5c62d79e6c57dfb32b0fbf9c018ef3aae8cc6
Nathanael Vieira <59123681+NathanaelV@users.noreply.github.com>
2022-08-23 16:45:28 -0300

    When message came nil

--- lib/rotakawasaki.rb
+++ lib/rotakawasaki.rb
@@ -7,5 +7,18 @@ require 'f1sales_custom/hooks'
 
 module Rotakawasaki
   class Error < StandardError; end
-  # Your code goes here...
+
+  class F1SalesCustom::Hooks::Lead
+    class << self
+      def switch_source(lead)
+        source_name = lead.source.name
+        message = lead.message&.downcase || ''
+        if message['campinas']
+          "#{source_name} - Campinas"
+        else
+          source_name
+        end
+      end
+    end
+  end
 end
--- spec/hook_spec.rb
+++ spec/hook_spec.rb
@@ -18,9 +18,18 @@ RSpec.describe F1SalesCustom::Hooks::Lead do
     end
 
     let(:source_name) { 'Facebook - Rota Kawasaki' }
+    let(:switch_source) { described_class.switch_source(lead) }
 
     it 'return with name Campinas' do
       expect(switch_source).to eq('Facebook - Rota Kawasaki - Campinas')
+    end
+
+    context 'when message comes empty' do
+      before { lead.message = nil }
+
+      it 'return the original source' do
+        expect(switch_source).to eq('Facebook - Rota Kawasaki')
+      end
     end
   end
 end
